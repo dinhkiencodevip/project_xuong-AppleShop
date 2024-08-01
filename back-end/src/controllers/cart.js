0291e33448616b1c75ccdc1056fa856ca095a7e2
dinhkiencodevip <15012004nguyendinhkien@gmail.com>
2024-08-01 22:02:29 +0700

    add .

--- back-end/src/controllers/cart.js
+++ back-end/src/controllers/cart.js
@@ -2,6 +2,12 @@ import Cart from "../models/Cart.js";
 import Product from "../models/Product.js";
 export const getCart = async (req, res, next) => {
   try {
+    const userId = res.userId;
+    const cart = await Cart.findOne({ userId }).populate("products.product");
+    return res.json({
+      message: "Get cart successfully",
+      cart,
+    });
   } catch (error) {
     next(error);
   }
@@ -12,25 +18,30 @@ export const addToCart = async (req, res, next) => {
     const userId = req.user._id;
     const { productId, quantity } = req.body;
     const product = await Product.findById(productId);
+    if (!product)
+      return res.status(404).json({
+        message: "Product not found",
+      });
     //Neu nguoi dung chua co cart thi tao cart, neu co roi thi them vao cart
 
     let cart = await Cart.findOne({ userId });
     if (!cart) cart = new Cart({ userId, products: [], totalPrice: 0 });
     const productIndex = cart.products.findIndex(
-      (item) => item.product === productId
+      (item) => item.product.toString() === productId
     );
     if (productIndex === -1) {
       // Neu san pham chua co trong cart.products thi push san pham vao cart kem theo quantity
       cart.products.push({ product: productId, quantity });
     } else {
+      cart.products[existingProductIndex].quantity += quantity;
       //Neu san pham da co trong cart roi ma an mua them thi cap nhat lai quantity
-      cart.totalPrice += product.price * quantity;
-      await cart.save();
-      return res.status(200).json({
-        message: "Them san pham vao gio hang thanh cong!",
-        cart,
-      });
     }
+    cart.totalPrice += product.price * quantity;
+    await cart.save();
+    return res.status(200).json({
+      message: "Them san pham vao gio hang thanh cong!",
+      cart,
+    });
   } catch (error) {
     next(error);
   }
@@ -38,6 +49,25 @@ export const addToCart = async (req, res, next) => {
 
 export const removeFormCart = async (req, res, next) => {
   try {
+    const userId = req.userId;
+    const { productId } = req.body;
+    let cart = await Cart.findOne({ userId });
+    if (!cart) return res.status(404).json({ message: "Cart not found" });
+    const findIndex = cart.products.findIndex(
+      (p) => p.product.toString() === productId
+    );
+    if (findIndex === -1)
+      return res.status(404).json({ message: "Product not found in cart" });
+    const product = cart.products[findIndex];
+    cart.totalPrice -= product.quantity * product.product.price;
+    cart.products = cart.products.filter(
+      (p) => p.product.toString() !== productId
+    );
+    await cart.save();
+    return res.status(200).json({
+      message: "Remove product from cart successfully",
+      cart,
+    });
   } catch (error) {
     next(error);
   }
@@ -45,6 +75,24 @@ export const removeFormCart = async (req, res, next) => {
 
 export const checkOut = async (req, res, next) => {
   try {
+    const userId = req.userId;
+    const cart = await Cart.findOne({ userId }).populate("products.product");
+    if (!cart)
+      return res.status(400).json({
+        message: "Cart is empty",
+      });
+    // pttt
+    const order = new Order({
+      user: userId,
+      products: cart.products,
+      totalPrice: cart.totalPrice,
+    });
+    await order.save();
+
+    //Xoa gio hang sau khi thanh toan
+    cart.products = [];
+    cart.totalPrice = 0;
+    await res.status(200).json({ message: "Checkout susccessfully" });
   } catch (error) {
     next(error);
   }
--- back-end/src/models/Cart.js
+++ back-end/src/models/Cart.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
 const CartItem = new mongoose.Schema({
-  product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
-  quantity: Number,
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: { type: Number, required: true, min: 1 },
 });
 
 const cartSchema = new mongoose.Schema(
